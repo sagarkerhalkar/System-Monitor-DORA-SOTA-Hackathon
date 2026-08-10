21194d68e36b2d79bafbe20a9f8cbc26dc1f4c2f
Sagar Kerhalkar <Sagarhatta@gmail.com>
2026-08-10 19:47:05 +0530

    Hackathon 3.55 - Enable EBS CSI add-on with Pod Identity

--- infra/terraform/eks.tf
+++ infra/terraform/eks.tf
@@ -122,3 +122,21 @@ resource "aws_eks_addon" "pod_identity_agent" {
 
   depends_on = [aws_eks_node_group.system]
 }
+
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name = aws_eks_cluster.this.name
+  addon_name   = "aws-ebs-csi-driver"
+
+  pod_identity_association {
+    service_account = "ebs-csi-controller-sa"
+    role_arn        = aws_iam_role.ebs_csi.arn
+  }
+
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "PRESERVE"
+
+  depends_on = [
+    aws_eks_addon.pod_identity_agent,
+    aws_iam_role_policy_attachment.ebs_csi_cluster_scoped,
+  ]
+}
